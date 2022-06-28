61a5ca3dc5e74a40432fdac961e3f8248ca741d2
Adrián Jiménez Cayetano <7901501@alumnos.ufv.es>
2022-06-28 17:17:08 +0200

    First commit

---
+++ AJC/src/main/java/ufv/dis/final2022/AJC/AjcApplication.java
@@ -0,0 +1,13 @@
+package ufv.dis.final2022.AJC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjcApplication.class, args);
+	}
+
+}
