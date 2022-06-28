a04d41269f21dab4ee1b1b2875be11f206c899aa
Adrián Jiménez Cayetano <7901501@alumnos.ufv.es>
2022-06-28 17:57:41 +0200

    POST final

--- AJC/src/main/java/ufv/dis/final2022/AJC/Controller.java
+++ AJC/src/main/java/ufv/dis/final2022/AJC/Controller.java
@@ -1,5 +1,6 @@
 package ufv.dis.final2022.AJC;
 
+import com.google.gson.Gson;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +18,39 @@ public class Controller {
     @PostMapping("/ip")
     public String BuscarIP(@RequestBody IPBuscada c){
         String ippuntos = c.getIp();
-        long iptransformada = aux.D
+        long iptransformada = aux.Dot2LongIP(ippuntos);
 
-        return c.getIp();
+        IPBuscada ipbuscada = new IPBuscada();
+        IPEncontrada ipencontrada = new IPEncontrada();
+
+        boolean encontrado = false;
+
+        for(int i = 0; i<ListaIPs.size(); i++){
+            if((iptransformada >= ListaIPs.get(i).getIp_from()) && (iptransformada <= ListaIPs.get(i).getIp_to())){
+                ipencontrada.setIP(ippuntos);
+                ipencontrada.setCountry_code(ListaIPs.get(i).getCountry_code());
+                ipencontrada.setRegion_code(ListaIPs.get(i).getRegion_name());
+                ipencontrada.setCity_name(ListaIPs.get(i).getCity_name());
+                ipencontrada.setZip_code(ListaIPs.get(i).getTime_zone());
+
+                ipbuscada.setIp(c.getIp());
+                ipbuscada.setMensaje("Ip encontrada");
+                IpsBuscadas.add(ipbuscada);
+
+                encontrado = true;
+
+                String ResponseSI = new Gson().toJson(ipencontrada);
+                return ResponseSI;
+            }
+
+        }
+
+        if(encontrado==false){
+            ipbuscada.setIp(c.getIp());
+            ipbuscada.setMensaje("Ip no encontrada");
+            IpsBuscadas.add(ipbuscada);
+        }
+        String ResponseNO = new Gson().toJson(ipbuscada);
+        return ResponseNO;
     }
 }
